0ce6e72ec69d86d0cadd66a7d5e732c2d61c5d73
Stenio Machado <stenio.silva@youxgroup.com.br>
2022-05-26 21:27:28 -0300

    Break the app on purpose

--- src/script.js
+++ src/script.js
@@ -4,7 +4,7 @@ const phoneLabelSpan = document.querySelector('.phone-label-span')
 const phoneField = document.getElementById('phone')
 
 document.querySelector('#phone-checkbox')
-  .addEventListener('change', function() {
+  .addEventListener('', function() {
     if (this.checked) {
       phoneLabelSpan.style.display = 'inline'
       phoneField.required = !isPhoneRequired
@@ -17,7 +17,7 @@ document.querySelector('#phone-checkbox')
   })
 
 document.querySelector('button[type="submit"]')
-  .addEventListener('click', function(event) {
+  .addEventListener('', function(event) {
     event.preventDefault()
     const firstNameField = document.getElementById('firstName')
     const lastNameField = document.getElementById('lastName')
